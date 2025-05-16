3d9ca1ce95cc0494b7954d01a3c7ab413a989457
Nicolas <nikinhaferezini14@gmail.com>
2025-05-16 16:34:15 -0300

    Final Project

--- src/paginas/Read/Read.jsx
+++ src/paginas/Read/Read.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
-import './Read.module.css';
+import './Read.Module.css';
 
 function Read() {
     const [data, setData] = useState({});
