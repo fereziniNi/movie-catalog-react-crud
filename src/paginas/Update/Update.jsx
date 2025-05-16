400482ce2d65397d6a474011c8edb583ec23cba8
Nicolas <nikinhaferezini14@gmail.com>
2025-05-16 16:29:14 -0300

    Final Project

--- src/paginas/Update/Update.jsx
+++ src/paginas/Update/Update.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
-import './Update.module.css';
+import './Update.Module.css';
 
 function Update() {
     const { id } = useParams();
